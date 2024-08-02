b854d773787a57f23418415142eb97dc71a94d50
Neha Hunnur <hunnurneha@gmail.com>
2024-08-02 16:14:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setFullName(`${firstName} ${lastName}`);
+  };
+
+  return (
+    <div className="App">
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          First Name:
+          <input
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Last Name:
+          <input
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      {fullName && <p>Full Name: {fullName}</p>}
+    </div>
+  );
+}
+
+export default App;
